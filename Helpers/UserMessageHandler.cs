7903c7fb02cf0d369164258a46d00c7ce7fde8b7
Sebin Suresh <sebinsuresh@gmail.com>
2023-11-08 19:02:50 -0600

    Append messages if they're recent

--- Helpers/UserMessageHandler.cs
+++ Helpers/UserMessageHandler.cs
@@ -2,20 +2,30 @@ namespace DisplayProject.Helpers;
 
 public class LabelUserMessageHandler : IUserMessageHandler
 {
-    private readonly Label messageLabel;
+    private const double ClearLabelTimeInSeconds = 0.1;
+    private readonly Label _messageLabel;
+    private DateTime _lastMessageReceived = DateTime.Now;
 
     public LabelUserMessageHandler(Control.ControlCollection Parent)
     {
-        messageLabel = new Label
+        _messageLabel = new Label
         {
             Text = "No presses detected",
             AutoSize = true,
         };
-        Parent.Add(messageLabel);
+        Parent.Add(_messageLabel);
     }
 
     public void ShowMessage(string message)
     {
-        messageLabel.Text = message;
+        if ((DateTime.Now - _lastMessageReceived).Seconds > ClearLabelTimeInSeconds)
+        {
+            _messageLabel.Text = message;
+        }
+        else
+        {
+            _messageLabel.Text += "\n" + message;
+        }
+        _lastMessageReceived = DateTime.Now;
     }
 }
